65327fa0df613b3aae6c9c0e13cbfeb27cb19d9d
Ashley Lago <ashl3ylag0@gmail.com>
2024-12-03 18:43:41 -0500

    Flushed out migrations and started on seeds

--- data/migrations/20241203225757_first-migration.js
+++ data/migrations/20241203225757_first-migration.js
@@ -2,14 +2,56 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema
+        .createTable('recipes', tbl => {
+            tbl.increments('recipe_id')
+            tbl.string('recipe_name', 128).unique().notNullable()
+        })
+        .createTable('steps', tbl => {
+            tbl.increments('step_id')
+            tbl.integer('step_number').notNullable()
+            tbl.string('step_instructions', 256).notNullable()
+            tbl.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+        })
+        .createTable('ingredients', tbl => {
+            tbl.increments('ingredient_id')
+            tbl.string('ingredient_name', 128).unique().notNullable()
+        })
+        .createTable('step_ingredients', tbl => {
+            tbl.increments('step_ingredient_id')
+            tbl.float('quantity').notNullable()
+            tbl.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+            tbl.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+        })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema
+        .dropTableIfExists('step_ingredients')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('steps')
+        .dropTableIfExists('recipes')
 };
--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/cook_book.db3'
     },
     migrations: {
       directory: './data/migrations'
