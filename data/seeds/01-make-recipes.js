74d68ab6fa4cd0f62e6c4d325022b214455742ab
Ashley Lago <ashl3ylag0@gmail.com>
2024-12-03 23:03:27 -0500

    Added new recipe

--- data/seeds/01-make-recipes.js
+++ data/seeds/01-make-recipes.js
@@ -1,7 +1,8 @@
 const recipes = [
     { recipe_name: "BBQ Chicken Sandwich" },
     { recipe_name: "Chicken Tacos" },
-    { recipe_name: "Grilled Chicken Quesadilla" }
+    { recipe_name: "Grilled Chicken Quesadilla" },
+    { recipe_name: "Spaghetti Bolognese" }
 ]
 
 const steps = [
@@ -24,7 +25,11 @@ const steps = [
     {recipe_id: 3, step_number: 2, step_instructions: "Fill the tortillas with shredded chicken"},
     {recipe_id: 3, step_number: 3, step_instructions: "Add shredded cheese"},
     {recipe_id: 3, step_number: 4, step_instructions: "Cook on a skillet until golden brown"},
-    {recipe_id: 3, step_number: 5, step_instructions: "Serve with salsa"}
+    {recipe_id: 3, step_number: 5, step_instructions: "Serve with salsa"},
+
+    // Spaghetti Bolognese
+    {recipe_id: 4, step_number: 1, step_instructions: "Put a large saucepan on a medium heat"},
+    {recipe_id: 4, step_number: 2, step_instructions: "Mix eggs and ham"}
 ]
 
 const ingredients = [
@@ -35,7 +40,9 @@ const ingredients = [
     {ingredient_name: "lettuce", ingredient_unit: "leaves"},
     {ingredient_name: "cheese", ingredient_unit: "oz"},
     {ingredient_name: "salsa", ingredient_unit: "tbs"},
-    {ingredient_name: "avocado", ingredient_unit: "slices"}
+    {ingredient_name: "avocado", ingredient_unit: "slices"},
+    {ingredient_name: "eggs", ingredient_unit: "whole"},
+    {ingredient_name: "ham", ingredient_unit: "slices"}
 ]
 
 const step_ingredients = [
@@ -57,7 +64,11 @@ const step_ingredients = [
     {step_id: 11, ingredient_id: 4, quantity: 2},
     {step_id: 12, ingredient_id: 1, quantity: 7},
     {step_id: 13, ingredient_id: 6, quantity: 2},
-    {step_id: 15, ingredient_id: 7, quantity: 2}
+    {step_id: 15, ingredient_id: 7, quantity: 2},
+    
+    // Spaghetti Bolognese
+    {step_id: 17, ingredient_id: 9, quantity: 2},
+    {step_id: 17, ingredient_id: 10, quantity: 1},
 ]
 
 exports.seed = async function(knex) {
